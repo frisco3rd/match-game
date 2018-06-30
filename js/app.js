d18ef2f813b9ba02427fcc362a7344849e42d396
Dwight Carter <carter91jr@gmail.com>
2018-06-30 07:38:29 -0400

    Fixed star display modal

--- js/app.js
+++ js/app.js
@@ -199,12 +199,13 @@ function toggleModal(){
 const modal = document.querySelector('.modal');
 let timerResults = document.querySelector('.timer-output').innerHTML;
 let movesResults = document.querySelector('.moves').innerHTML;
-let starsResults = document.querySelector('.stars').write;
+let starsResults = document.querySelector('.stars').innerHTML;
 
 	document.querySelector('.modal_time').innerHTML = `Time = ${timerResults}`;
-	document.querySelector('.modal_stars').write = `Stars = ${starsResults}`;
+	document.querySelector('.modal_stars').innerHTML = `Stars = ${starsResults}`;
 	document.querySelector('.modal_moves').innerHTML = `Moves = ${movesResults}`;
 
-	modal.style.display ='inline';
 
 }
+
+
